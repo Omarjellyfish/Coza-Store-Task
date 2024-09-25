2a6413307bdaf6dcb83f30ad087c2be665bd95fc
Omarjellyfish <omarkandilfan@gmail.com>
2024-09-25 20:06:27 +0300

    card creation and intialization

--- CozaStore/src/components/Card/Card.jsx
+++ CozaStore/src/components/Card/Card.jsx
@@ -1,4 +1,22 @@
 import './Card.css'
-export default function Card(){
-    
+export default function Card({img,cat,name,price}){
+    return(
+        <>
+            <div className="card">
+                <div className="product-image">
+                    <img src={img} alt={name} />
+                    <button className="quick-view">Quick View</button>
+                </div>
+                <div className='description'>
+                    <div className="product">
+                        <p className="product-name">
+                            {name}
+                        </p>
+                        <i className="favorite" id='fav'/>
+                    </div>
+                    <p className="price">${price}</p>
+                </div>
+            </div>
+        </>
+    )
 }
